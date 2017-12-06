a681b06efaf484df99fe0f52b21531591d37376c
Mauricio Flor <duvamalicio3d@hotmail.com>
2017-12-05 20:48:54 -0500

    Resolving to Database @Mauricioft N°2

--- schema.js
+++ schema.js
@@ -46,9 +46,9 @@ const typeDefs = `
 const resolvers = {
   Query: {
     cursos: () => Curso.query().eager('[profesor, comentarios]'),
-    profesores: () => Profesor.query(),
-    curso: (rootValue, args) => Curso.query(),
-    profesor: (rootValue, args) => Profesor.query()
+    profesores: () => Profesor.query().eager('[cursos]'),
+    curso: (rootValue, args) => Curso.query().eager('[profesor, comentarios]').findById(args.id),
+    profesor: (rootValue, args) => Profesor.query().eager('[cursos]').findById(args.id),
   },
 }
 
